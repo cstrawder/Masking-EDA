a87dc8ee8a2f2eb964e664ba1e3a12363683588b
Chelsea Strawder <chelsea.strawder@alleninstitute.org>
2020-07-13 17:32:56 -0700

    created 2 new files

--- create_pdf.py
+++ create_pdf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jun 30 10:59:57 2020
+Created on Mon Jul 13 14:54:31 2020
 
 @author: chelsea.strawder
 """
@@ -17,10 +17,13 @@ from percentCorrect import session_stats
 from catchTrials import catch_trials
 import matplotlib.pyplot as plt
 import os
-from fpdf import FPDF
+import reportlab
+from reportlab.pdfgen import canvas
+from reportlab.platypus import Image
+from reportlab.lib.units import inch
+from reportlab.lib.pagesizes import letter
 
 
-# choose mouse file
 d = dataAnalysis.import_data()
 
 mouse_id=d['subjectName'][()]
@@ -30,71 +33,55 @@ date = date[:2]+'-'+date[2:]
 date = date if date[:2] in ['10','11','12'] else date[-4:]
 
 directory = r'\\allen\programs\braintv\workgroups\nc-ophys\corbettb\Masking\active_mice'
-dataDir = os.path.join(os.path.join(directory, mouse_id), 'Plots') 
+dataDir = os.path.join(os.path.join(directory, mouse_id), 'Plots/') 
 
-pdf = FPDF('P', 'in', 'A4')
-pdf.set_margins(left=.2, top=.5, right=.2)
-pdf.add_page()
-pdf.set_font('Arial', 'B', 14)
-pdf.cell(.5,1,'Mouse')
+c = canvas.Canvas(mouse_id + ' Daily Summary ' + date + '.pdf', pagesize=letter, bottomup=1)
 
 
-wheelDir = os.path.join(dataDir, 'Wheel plots')
-# daily wheel 
-behaviorAnalysis.makeWheelPlot(d, responseFilter=[-1,0,1], 
-                               ignoreRepeats=True, framesToShowBeforeStart=60, 
-                               mask=False, maskOnly=False, xlim=[0, .8], ylim='auto', figsize=[6.5, 4.7])
 
-plt.rcParams.update({'font.size': 8})
-plt.subplots_adjust(top=0.9, bottom=0.1, left=0.1, right=0.96)
+# insert "Daily Summary" at top bold, mouse id, and date 
+# insert daily wheel plot on left of canvas
+reportlab.platypus.Image(dataDir + mouse_id + ' ' + date + '.png', width=6*inch, height=4.5*inch).drawOn(c, 0, 10*inch)
 
-plt.savefig(wheelDir+'/Daily Wheel/' + mouse_id + ' ' + date + '.png', dpi=300, bbox_inches='tight')
-
-pdf.image(wheelDir+'/Daily Wheel/' + mouse_id + ' ' + date + '.png', x = 1, 
-          y = 1, w = 6, h = 4, type = '', link = '')
+# insert textbox with daily summary to right of plot - use textObject 
+        # set text origin 6.25 from left, .9 from top (inches)
+# no response wheel under daily wheel, same size
+# word "No Response" next to plot 
+# break
+c.showPage()
 
 
-# prints out performance counts/% from session
-session_stats(d)
+# insert catch trial wheel trace plot top left 
+# insert textbox to right of plot with catch trial counts
+# insert session plot - takes up entire bottom half, is already perfect size 
+# break
+c.showPage()
+
+# insert frame dist plot in upper left 1/6 
+# insert frame intervals plot in upper right 1/6 (same size)
+# insert wheel pos dist plot underneath 
+# break
+c.showPage()
 
 
-# plot no response trials only (with repeats)
-behaviorAnalysis.makeWheelPlot(d, responseFilter=[0], 
-                               ignoreRepeats=False, framesToShowBeforeStart=60, 
-                               mask=False, maskOnly=False,  xlim=[0, .8], ylim=[-10,10], figsize=[6.2,4.7])
+# insert qVio sum plot top left 2/3s
+# insert textbox in top right 1/3 with Qvios and max
+# insert qVio count below qVio sum, same size 
+# save 
 
-plt.rcParams.update({'font.size': 10})
-plt.subplots_adjust(top=0.9, bottom=0.1, left=0.1, right=0.96)
-
-plt.savefig(wheelDir+'/No Resp Wheel/' + mouse_id + ' ' + date + ' no resp.png', dpi=300, bbox_inches='tight')
+c.showPage()
+c.save()
 
 
-pdf.add_page()
-
-# plots catch trial wheel traces 
-catch_trials(d, xlim=[0, .8]) 
-
-plt.savefig(wheelDir+'/Catch/' + mouse_id + ' catch ' + date + '.png', dpi=300, bbox_inches='tight')
 
 
-# plot activity over entire session, trial-by-trial - 1 plot
-plot_session(d)
-plt.savefig(dataDir + '/Session plots/' + mouse_id + ' session ' + date + '.png', dpi=300, bbox_inches='tight')
 
 
-pdf.add_page()
-
-# plots frame distribution and frame intervals 
-qualityControl.check_frame_intervals(d)
 
 
-# check number of quiescent period violations - use 'sum' for cumsum OR 'count' for count per trial
-qualityControl.check_qviolations(d, plot_type='sum')
 
-qualityControl.check_qviolations(d, plot_type='count')
 
-# check distribution of delta wheel position 
-qualityControl.check_wheel(d)
+
 
 
 
---
+++ save_images.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 30 10:59:57 2020
+
+@author: chelsea.strawder
+"""
+
+import behaviorAnalysis
+import performanceBySOA
+from plottingTargetContrast import plot_contrast
+from plotting_target_lengths import plot_flash
+from SessionPerformance import plot_session
+from responsePlotByParam import plot_by_param
+import dataAnalysis
+import qualityControl
+from percentCorrect import session_stats
+from catchTrials import catch_trials
+import matplotlib.pyplot as plt
+import os
+from fpdf import FPDF
+
+
+# choose mouse file
+d = dataAnalysis.import_data()
+
+mouse_id=d['subjectName'][()]
+date = d['startTime'][()].split('_')[0][-4:]
+date = date[:2]+'-'+date[2:]
+
+date = date if date[:2] in ['10','11','12'] else date[-4:]
+
+directory = r'\\allen\programs\braintv\workgroups\nc-ophys\corbettb\Masking\active_mice'
+dataDir = os.path.join(os.path.join(directory, mouse_id), 'Plots') 
+
+
+
+pdf = FPDF('P', 'in', 'A4')
+pdf.set_margins(left=.2, top=.5, right=.2)
+pdf.add_page()
+pdf.set_font('Arial', 'B', 14)
+pdf.cell(.5,1,'Mouse')
+
+
+wheelDir = os.path.join(dataDir, 'Wheel plots')
+# daily wheel 
+behaviorAnalysis.makeWheelPlot(d, responseFilter=[-1,0,1], 
+                               ignoreRepeats=True, framesToShowBeforeStart=60, 
+                               mask=False, maskOnly=False, xlim=[0, .8], ylim='auto', figsize=[6.5, 4.7])
+
+plt.rcParams.update({'font.size': 8})
+plt.subplots_adjust(top=0.9, bottom=0.1, left=0.1, right=0.96)
+
+plt.savefig(wheelDir+'/Daily Wheel/' + mouse_id + ' ' + date + '.png', dpi=300, bbox_inches='tight')
+
+pdf.image(wheelDir+'/Daily Wheel/' + mouse_id + ' ' + date + '.png', x = 1, 
+          y = 1, w = 6, h = 4, type = '', link = '')
+
+
+# prints out performance counts/% from session
+session_stats(d)
+
+
+# plot no response trials only (with repeats)
+behaviorAnalysis.makeWheelPlot(d, responseFilter=[0], 
+                               ignoreRepeats=False, framesToShowBeforeStart=60, 
+                               mask=False, maskOnly=False,  xlim=[0, .8], ylim=[-10,10], figsize=[6.2,4.7])
+
+plt.rcParams.update({'font.size': 10})
+plt.subplots_adjust(top=0.9, bottom=0.1, left=0.1, right=0.96)
+
+plt.savefig(wheelDir+'/No Resp Wheel/' + mouse_id + ' ' + date + ' no resp.png', dpi=300, bbox_inches='tight')
+
+
+pdf.add_page()
+
+# plots catch trial wheel traces 
+catch_trials(d, xlim=[0, .8]) 
+
+plt.savefig(wheelDir+'/Catch/' + mouse_id + ' catch ' + date + '.png', dpi=300, bbox_inches='tight')
+
+
+# plot activity over entire session, trial-by-trial - 1 plot
+plot_session(d)
+plt.savefig(dataDir + '/Session plots/' + mouse_id + ' session ' + date + '.png', dpi=300, bbox_inches='tight')
+
+
+pdf.add_page()
+
+# plots frame distribution and frame intervals 
+qualityControl.check_frame_intervals(d)
+
+
+# check number of quiescent period violations - use 'sum' for cumsum OR 'count' for count per trial
+qualityControl.check_qviolations(d, plot_type='sum')
+
+qualityControl.check_qviolations(d, plot_type='count')
+
+# check distribution of delta wheel position 
+qualityControl.check_wheel(d)
+
+
+
+
